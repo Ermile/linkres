36331eb90a662cd9e3f6e8907eaf8adb3819bbb2
Javad <J.Evazzadeh@live.com>
2016-04-05 09:05:21 +0430

    update to work with last chnages

--- public_html/index.php
+++ public_html/index.php
@@ -3,14 +3,6 @@
  @ In the name Of Allah
 **/
 
-// if config exist, require it else show related error message
-if ( file_exists( __DIR__ . '/config.php') )
-	require_once( __DIR__ . '/config.php');
-else
-{   // A config file doesn't exist
-	echo( "<p>There doesn't seem to be a <code>config.php</code> file. I need this before we can get started.</p>" );
-	exit();
-}
 // if Saloos exist, require it else show related error message
 if ( file_exists( '../../saloos/autoload.php') )
 {
